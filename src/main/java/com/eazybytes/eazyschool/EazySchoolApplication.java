3c0f673761df086792a534f24433bbd7485ce8c4
hatef khodkar <hatef.khodkar@hotmail.com>
2023-03-04 21:31:19 +0330

    add spring dev-tool and dynamic web content

--- src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
+++ src/main/java/com/eazybytes/eazyschool/EazySchoolApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EazySchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EazySchoolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EazySchoolApplication.class, args);
+    }
 }
